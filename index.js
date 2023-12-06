467720744ecf9f95f13218157b00839466fd938b
Daniel Riverol <dannyriverol@hotmail.com>
2023-12-06 19:30:21 -0300

    Configurando variables de entorno

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Cambia a tu URL de frontend
+    origin:process.env.FRONT_URL, // Cambia a tu URL de frontend
     credentials: true,
   })
 );
